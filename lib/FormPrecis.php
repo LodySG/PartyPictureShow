09bd4108a015b0c4d65d5da119d74a6a7be4a835
LodySG <lodysg@gmail.com>
2014-12-11 23:48:58 +0100

    premier version

--- lib/FormPrecis.php
+++ lib/FormPrecis.php
@@ -11,22 +11,43 @@
  *
  * @author lodydody
  */
+
+require 'lib/Form.php';
+
 class FormPrecis {
     
-    public static function userRegisteration($adressmac){
-       $form = new Form($adressmac,'POST');
-       $form->add('text','pseudo');
-       $form->add('submit','Ceci est mon nom');
+    public static function userRegisteration($macadress){
+       $form = new Form('user_recorder','POST');
+       
+       $form    ->add('Text','pseudo');
+       
+       $form    ->add('Hidden','macadress')
+                ->value($macadress);
+       
+       $form    ->add('Submit','submit')
+                ->value('Ceci est mon nom');
+       
        return $form;
     }
     
+    
     public static function imageUpload(){
-        $form = new Form('image','POST');
+        $form = new Form('image_uploader','POST');
+        
+        $form   ->add('File','image')
+                ->max_size(4000000)
+                ->filter_extensions(array('jpg', 'gif', 'png'));
+        
+        $form   ->add('Submit', 'submit')
+                ->value('Tiens !!!');
+        
         return $form;
     }
     
+    
     public static function partySetUp(){
         $form = new Form();
+        
         return $form;
     }
 
